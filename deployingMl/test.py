66b281b692cae9d6dff3738a3e6252bf401c22f3
İBRAHİM MAMO <118085530+mamoibrahim@users.noreply.github.com>
2025-03-25 22:54:17 +0300

    Delete deployingMl directory

--- deployingMl/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/predict/"
-data = {"features": [5.1, 3.5, 1.4, 0.2]}  # ✅ Dictionary format
-
-response = requests.post(url, json=data)  # ✅ Use json=, NOT data=
-print(response.json())  # Should return the prediction
